23eee56273ceed99d9a6b9ead4cc494797353104
Andrew Lima <andrew@arctek.co.za>
2024-11-05 15:32:27 +0200

    Escape and sanitize "src" blocks as well.

--- blocks/src/testimonials-name/render.php
+++ blocks/src/testimonials-name/render.php
@@ -1,7 +1,7 @@
 <?php
 $testimonial = new PMPro_Testimonial( get_the_ID() );
 if ( $testimonial->get_name() ) { ?>
-<div <?php echo get_block_wrapper_attributes(); ?> itemprop="author" itemscope itemtype="http://schema.org/Person">
-	<span itemprop="name"><?php echo $testimonial->get_name( $attributes['linkEnabled'] ); ?></span>
+<div <?php echo esc_attr( get_block_wrapper_attributes() ); ?> itemprop="author" itemscope itemtype="http://schema.org/Person">
+	<span itemprop="name"><?php echo wp_kses_post( $testimonial->get_name( $attributes['linkEnabled'] ) ); ?></span>
 </div>
 <?php } ?>
