0bc890e20e2747ec9150783bd5ebfa7ad5a9659e
Derek Ashauer <derek@ashwebstudio.com>
2025-02-28 13:58:16 -0700

    consistency in function usage

--- includes/post-types/testimonials.php
+++ includes/post-types/testimonials.php
@@ -166,7 +166,7 @@ function pmpro_testimonials_meta_box() {
 							} else {
 								$checked = 'false';
 							}
-							$class = implode( ' ', array_unique( $classes ) );
+							$class = join( ' ', $classes );
 							$label = sprintf( esc_html__( '%s Star Rating', 'pmpro-testimonials' ), $i );
 							echo '<svg role="radio" aria-checked="' . esc_attr( $checked ) . '" aria-label="' . esc_attr( $label ) . '" data-value="' . esc_attr( $i ) . '" class="' . esc_attr( $class ) . '" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
 								<polygon points="12 17.27 18.18 21 16.54 13.97 22 9.24 14.81 8.63 12 2 9.19 8.63 2 9.24 7.46 13.97 5.82 21 12 17.27" />
