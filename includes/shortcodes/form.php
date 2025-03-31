2201694e2827ffcfcc559f55c89e075e90247b9c
Andrew Lima <andrew@arctek.co.za>
2025-03-31 11:15:20 +0200

    Fix the set_transient logic.

--- includes/shortcodes/form.php
+++ includes/shortcodes/form.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Shortcode to output a form for submitting testimonials.
- * NOTE: The display method will process the form.
+ * NOTE: The display method will also process the form.
  * 
  * @param array $atts Shortcode attributes for categories, tags, required fields, and dropdown display.
  */
@@ -24,10 +24,9 @@ function pmpro_testimonials_shortcode_form( $atts ) {
 		'pmpro_testimonials_form'
 	);
 
-	// Let's save the attributes in the cache for if we process the form, for the required_fields
-	if ( ! empty( $atts['required_fields'] ) ) {
-		set_transient( 'pmpro_testimonials_form_required_fields_' . get_the_ID(), $atts, 10 * MINUTE_IN_SECONDS );
-	}
+	// Let's save the attributes in the cache for if we process the form, for the required_fields. 
+	// Note: This transient is reset whenever set_transient is called - we do not need to handle/delete this transient when attributes change.
+	set_transient( 'pmpro_testimonials_form_required_fields_' . get_the_ID(), $atts, 10 * MINUTE_IN_SECONDS );
 
 	$form = new PMPro_Testimonial_Form( $atts );
 	return $form->display( false );
